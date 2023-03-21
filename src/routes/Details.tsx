fef7dce285dbc21a18a2e00dc2b602146c80e671
Tim Man <timothyc.man@gmail.com>
2023-03-21 20:04:08 +0800

    style: style details page

--- src/routes/Details.tsx
+++ src/routes/Details.tsx
@@ -1,45 +1,54 @@
-import { Link, useSearchParams } from "react-router-dom";
+import { NavLink, useSearchParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { TokenData } from "../data/gammaApi";
 import useGammaCollectionToken from "../hooks/useGammaCollectionsToken";
+import "./Details.scss";
 
 function Details() {
   const [searchParams] = useSearchParams();
   const collectionName = searchParams.get("collectionName");
   const tokenId = searchParams.get("tokenId");
   const { data, error } = useGammaCollectionToken({ collectionName, tokenId });
-  console.log(data, error);
-  console.log(collectionName, tokenId);
+
+  console.log(error); // TODO use this error
 
   return (
-    <div>
-      <header>
-        <Link to={".."}>{"<"}</Link>
+    <div className="details">
+      <header className="details__header">
+        <NavLink to={".."}>
+          <FontAwesomeIcon icon={faChevronLeft} size="lg" />
+        </NavLink>
         <h1>NFT Details</h1>
-        {data && (
-          <div>
-            <img
-              src={parseImageUrl(data?.data)}
-              alt={data?.data?.token_metadata?.name}
-            />
+      </header>
+      {data && (
+        <div className="details__body">
+          <img
+            src={parseImageUrl(data?.data)}
+            alt={data?.data?.token_metadata?.name}
+          />
+          <section className="item-details">
             <h2>Item Details</h2>
             <hr />
             <div>
-              <span>Identifier</span>
+              <h3>Identifier</h3>
               <span>{data?.data?.fully_qualified_token_id}</span>
             </div>
+            <h2>Attributes</h2>
             {data?.data?.nft_token_attributes?.map((attribute) => (
               <Attribute
                 label={attribute?.trait_type}
                 value={attribute?.value}
               />
             ))}
-          </div>
-        )}
-      </header>
+          </section>
+        </div>
+      )}
     </div>
   );
 }
 
+// TODO would unit test this
 const parseImageUrl = (tokenData: TokenData): string => {
   if (!tokenData?.token_metadata?.image_url) {
     return "";
@@ -56,7 +65,7 @@ const parseImageUrl = (tokenData: TokenData): string => {
 
 const Attribute = ({ label, value }: { label: string; value: string }) => (
   <div>
-    <span>{label}</span>
+    <h3>{label}</h3>
     <span>{value}</span>
   </div>
 );
